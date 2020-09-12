4bcb7dc2e270a075c6dde80f07b09ad4f9200153
Marek Grota <marekgro@gmail.com>
2020-09-12 21:14:07 +0200

    Simple search engine switch statement

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -13,23 +13,42 @@ public class Main {
         for (int i = 0; i < numberOfPeople; i++) {
             database[i] = scanner.nextLine();
         }
-
-        System.out.println("Enter the number of search queries:");
-        int queries = scanner.nextInt();
-
-        for (int i = 0; i < queries; i++) {
-            System.out.println("Enter data to search people:");
-            String toSearch = scanner.next().toLowerCase();
-            find(database, toSearch);
-            System.out.println();
-        }
+        boolean loop = false;
+        do {
+            System.out.println("\n=== Menu ===");
+            System.out.println("1. Find a person");
+            System.out.println("2. Print all people");
+            System.out.println("0. Exit");
+            int decision = scanner.nextInt();
+            switch (decision) {
+                case 1: {
+                    System.out.println("Enter a name or email to search all suitable people.");
+                    String toSearch = scanner.next().toLowerCase();
+                    find(database, toSearch);
+                    System.out.println();
+                    break;
+                }
+                case 2: {
+                    System.out.println("=== List of people ===");
+                    for (int i = 0; i < numberOfPeople; i++) {
+                        System.out.println(database[i]);
+                    }
+                    break;
+                }
+                case 0:
+                    loop = true;
+                    System.out.println("\nBye!");
+                    break;
+                default:
+                    System.out.println("Incorrect option! Try again.");
+            }
+        } while (!loop);
     }
-
 
     static void find(String[] strings, String toSearch) {
         boolean check = false;
         for (String string : strings) {
-            if (string.contains(toSearch)) {
+            if (string.toLowerCase().contains(toSearch)) {
                 System.out.println(string);
                 check = true;
             }
